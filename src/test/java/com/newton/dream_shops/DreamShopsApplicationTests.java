86b0256dda6b34e91def5eec37bc383399bda1af
newton <wamitinewton@gmail.com>
2025-05-22 16:23:47 -0400

    migrated from local to neon db

--- src/test/java/com/newton/dream_shops/DreamShopsApplicationTests.java
+++ src/test/java/com/newton/dream_shops/DreamShopsApplicationTests.java
@@ -3,11 +3,7 @@ package com.newton.dream_shops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DreamShopsApplication.class)
 class DreamShopsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }

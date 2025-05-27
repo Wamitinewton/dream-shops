628f8b9f85e6f94b36d63ba7cf49ff5bc1a70ab2
newton <wamitinewton@gmail.com>
2025-05-27 03:28:29 -0400

    added category search mechanism

--- src/main/java/com/newton/dream_shops/repository/category/CategoryRepository.java
+++ src/main/java/com/newton/dream_shops/repository/category/CategoryRepository.java
@@ -1,10 +1,20 @@
 package com.newton.dream_shops.repository.category;
 
 import com.newton.dream_shops.models.category.Category;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
     Category findByName(String name);
 
     boolean existsByName(String name);
+
+    @Query("SELECT c FROM Category c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    List<Category> searchByName(@Param("searchTerm") String searchTerm);
 }
+
